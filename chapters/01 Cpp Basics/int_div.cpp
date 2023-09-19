76a0e8621abacc0c5bc8d70c27a44f20a4184b4f
GrzegorzManiak <git@grzegorz.ie>
2023-09-19 08:43:32 +0100

    23

---
+++ chapters/01 Cpp Basics/int_div.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a(5),
+        b(1);
+
+    cout << a / b << " - 1 \n";
+    cout << b / a << "\n";
+
+    cout << a % b << " - 2 \n";
+    cout << b % a << "\n";
+
+    double c = b / a;
+    cout << c << " - 3 \n";
+    
+    c = (double)b / a;
+    cout << c << " - 4 \n";
+
+    c = b / (double)a;
+    cout << c << " - 5 \n";
+
+    // -- Aslong as one side of the division is a double,
+    //    it will divide correctly.
+
+    return 0;
+}
